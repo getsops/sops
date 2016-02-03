96b4bc34b8c0aecade310856861b623751f45bf1
Julien Vehent <julien@linuxwall.info>
2016-02-03 13:22:33 -0500

    don't encrypt empty values, fixes #42

--- sops/__init__.py
+++ sops/__init__.py
@@ -38,7 +38,7 @@ else:
 if sys.version_info[0] == 3:
     raw_input = input
 
-VERSION = 1.5
+VERSION = 1.6
 
 DESC = """
 `sops` supports AWS KMS and PGP encryption:
@@ -795,6 +795,9 @@ def walk_list_and_encrypt(branch, key, aad=b'', stash=None, digest=None):
 
 def encrypt(value, key, aad=b'', stash=None, digest=None):
     """Return an encrypted string of the value provided."""
+    if not value:
+        # if the value is empty, return it as is, don't encrypt
+        return ""
     # save the original type
     # the order in which we do this matters. For example, a bool
     # is also an int, but an int isn't a bool, so we test for bool first
