8206d79d7a62f44ba6d6bd688b95d7a17c8939ee
Julien Vehent <julien@linuxwall.info>
2016-03-31 17:02:35 -0400

    Add check for stdout before refencing fd, fixes #57

--- sops/__init__.py
+++ sops/__init__.py
@@ -1177,11 +1177,14 @@ def write_file(tree, path=None, filetype=None):
     if path:
         if path != 'stdout':
             fd = open(path, "wb")
+        else:
+            fd = None
     else:
         fd = tempfile.NamedTemporaryFile(suffix="."+filetype, delete=False)
         path = fd.name
 
-    if not isinstance(tree, dict) and not isinstance(tree, list):
+    if fd and not isinstance(tree, dict) and not isinstance(tree, list):
+        # Write the entire tree to file descriptor
         fd.write(tree.encode('utf-8'))
         fd.close()
         return path
