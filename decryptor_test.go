bdc126d041016119f9496486c9bcdf4eedbaa8bc
Adrian Utrilla <adrianutrilla@gmail.com>
2016-08-02 14:02:01 -0700

    Added AES decryptor

---
+++ decryptor_test.go
@@ -0,0 +1,19 @@
+package sops
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestDecrypt(t *testing.T) {
+	expected := "foo"
+	key := strings.Repeat("f", 32)
+	message := `ENC[AES256_GCM,data:xPxG,iv:kMAhrJOMitZZP3C71cA1wnp543hHYFd8+Tv01hdEOqc=,tag:PDVgtlbfBU7A33NKugzNBg==,type:bytes]`
+	decryption, err := Decrypt(message, key)
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+	if decryption != expected {
+		t.Errorf("Decrypt(\"%s\", \"%s\") == \"%s\", expected %s", message, key, decryption, expected)
+	}
+}
