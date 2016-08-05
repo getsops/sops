ebfb234f737c4bd372df0ed399d8961713096353
Adrian Utrilla <adrianutrilla@gmail.com>
2016-08-05 09:25:05 -0700

    Added json decryption backend

--- storage.go
+++ storage.go
@@ -1,10 +1,13 @@
 package sops
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
+	"time"
+
 	"github.com/autrilla/sops/decryptor"
 	"gopkg.in/yaml.v2"
-	"time"
 )
 
 type Store interface {
@@ -98,4 +101,70 @@ func (store YAMLStore) Metadata(in string) (SopsMetadata, error) {
 		return sops, err
 	}
 	return sops, nil
+}
+
+func (store JSONStore) Metadata(in string) (SopsMetadata, error) {
+	var metadata SopsMetadata
+	var encoded map[string]interface{}
+	err := json.Unmarshal([]byte(in), &encoded)
+	if err != nil {
+		return metadata, err
+	}
+
+	sopsJson, err := json.Marshal(encoded["sops"])
+	if err != nil {
+		return metadata, err
+	}
+
+	err = json.Unmarshal(sopsJson, &metadata)
+	if err != nil {
+		return metadata, err
+	}
+	return metadata, nil
+}
+
+func (store JSONStore) DecryptValue(in interface{}, key string) (interface{}, error) {
+	switch in := in.(type) {
+	case map[string]interface{}:
+		return store.DecryptMap(in, key)
+	case string:
+		k, err := decryptor.Decrypt(in, key)
+		if err != nil {
+			return nil, fmt.Errorf("Could not decrypt \"%s\"", in)
+		}
+		return k, nil
+	default:
+	}
+	return nil, fmt.Errorf("Value %s is of unknown type", in)
+}
+
+func (store JSONStore) DecryptMap(in map[string]interface{}, key string) (map[string]interface{}, error) {
+	out := make(map[string]interface{})
+	for i, v := range in {
+		v, err := store.DecryptValue(v, key)
+		if err != nil {
+			return nil, err
+		}
+		out[i] = v
+	}
+
+	return out, nil
+}
+
+func (store JSONStore) Decrypt(in, key string) (string, error) {
+	var encoded map[string]interface{}
+	err := json.Unmarshal([]byte(in), &encoded)
+	if err != nil {
+		return "", err
+	}
+
+	v, err := store.DecryptMap(encoded, key)
+	if err != nil {
+		return "", err
+	}
+	j, err := json.Marshal(v)
+	if err != nil {
+		return "", err
+	}
+	return string(j), nil
 }
--- storage_test.go
+++ storage_test.go
@@ -46,3 +46,30 @@ func TestYamlMetadata(t *testing.T) {
 		t.Error("version should be %s, was %s", expected_version, sops.Version)
 	}
 }
+
+func TestJsonMetadata(t *testing.T) {
+	file, err := os.Open("test_resources/example.json")
+	defer file.Close()
+	in, err := ioutil.ReadAll(file)
+	sops, err := JSONStore{}.Metadata(string(in))
+	expected_version := "1.13"
+	if err != nil {
+		t.Error("json parsing thew error: %s", err)
+	}
+	if sops.Version != expected_version {
+		t.Error("version should be %s, was $s", expected_version, sops.Version)
+	}
+}
+
+func TestDecryptSimpleJSON(t *testing.T) {
+	json := `{"foo": "ENC[AES256_GCM,data:xPxG,iv:kMAhrJOMitZZP3C71cA1wnp543hHYFd8+Tv01hdEOqc=,tag:PDVgtlbfBU7A33NKugzNBg==,type:bytes]"}`
+	key := strings.Repeat("f", 32)
+	expected := "{\"foo\":\"foo\"}"
+	decryption, err := JSONStore{}.Decrypt(json, key)
+	if err != nil {
+		t.Error("Decryption failed: %s", err)
+	}
+	if decryption != expected {
+		t.Errorf("Decryption does not match expected result: %q != %q", decryption, expected)
+	}
+}
