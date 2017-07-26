fbc9b393a284031dc7ed5287283904e65eaf8ca4
Andy Freeland <andy@andyfreeland.net>
2017-07-25 20:31:46 -0700

    Unpin cryptography requirement
    Doesn't seem like there's a reason not to use newer versions of
    cryptography and it has the advantage of being much easier to install
    with wheels.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     install_requires=[
         'ruamel.yaml==0.11.7',
         'boto3>=1.1.3',
-        'cryptography==1.4',
+        'cryptography>=1.4',
         'setuptools>=18.8.1',
         'ordereddict>=1.1',
         'simplejson>=3.8'],
