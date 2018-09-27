2a1ec1a05f03db7fbc8d3234913ae7706cd547df
Adrian Utrilla <autrilla@mozilla.com>
2018-09-27 09:38:36 +0200

    Release 3.1.1
    Signed-off-by: Adrian Utrilla <autrilla@mozilla.com>

--- cmd/sops/version.go
+++ cmd/sops/version.go
@@ -10,7 +10,7 @@ import (
 	"gopkg.in/urfave/cli.v1"
 )
 
-const version = "3.1.0"
+const version = "3.1.1"
 
 func printVersion(c *cli.Context) {
 	out := fmt.Sprintf("%s %s", c.App.Name, c.App.Version)
