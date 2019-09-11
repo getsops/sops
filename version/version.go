045a876841d2498f3ea310e2e458c2e443ee3f69
AJ Bahnken <aj@ajvb.me>
2019-09-11 13:40:01 -0700

    Bump version and CHANGELOG for 3.4.0

--- version/version.go
+++ version/version.go
@@ -11,7 +11,7 @@ import (
 )
 
 // Version represents the value of the current semantic version
-const Version = "3.3.1"
+const Version = "3.4.0"
 
 // PrintVersion handles the version command for sops
 func PrintVersion(c *cli.Context) {
