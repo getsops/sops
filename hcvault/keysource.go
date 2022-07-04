c7ae3eee59ad79bf9a51328637fb5f3482495c6d
Hidde Beydals <hello@hidde.co>
2022-07-04 23:46:43 +0200

    hcvault: update API, add tests, tidy
    This updates the Vault API and client to latest, adds more extensive
    test coverage, and general tidying of bits of code.
    The improvements are based on a fork of the key source in the Flux
    project's kustomize-controller, built due to SOPS' limitation around
    credential management without relying on runtime environment variables.
    
    - Vault API and client have been updated to latest.
    - It introduces a `Token` type which holds a Vault token, and can be
      applied to the `MasterKey`. When applied, the token is used in the
      Vault client configuration, instead of relying on the `VAULT_TOKEN`
      environment variables, or the `.vault-token` file in the user's home
      directory. This is most useful when working with SOPS as an SDK, in
      combination with e.g. a local key service server implementation.
    - Extensive test coverage.
    
    The forked version of this has compatability tests to ensure it works
    with current SOPS:
    
    - https://github.com/fluxcd/kustomize-controller/blob/62fb2d96a297c7e344050f46ee54074ef66dd438/internal/sops/hcvault/keysource_test.go#L130
    - https://github.com/fluxcd/kustomize-controller/blob/62fb2d96a297c7e344050f46ee54074ef66dd438/internal/sops/hcvault/keysource_test.go#L202
    
    Signed-off-by: Hidde Beydals <hello@hidde.co>

--- hcvault/keysource.go
+++ hcvault/keysource.go
@@ -3,6 +3,7 @@ package hcvault
 import (
 	"bytes"
 	"encoding/base64"
+	"errors"
 	"fmt"
 	"io"
 	"net/url"
@@ -14,34 +15,66 @@ import (
 	"time"
 
 	"github.com/hashicorp/vault/api"
-	homedir "github.com/mitchellh/go-homedir"
+	"github.com/mitchellh/go-homedir"
 	"github.com/sirupsen/logrus"
+
 	"go.mozilla.org/sops/v3/logging"
 )
-
-var log *logrus.Logger
 
 func init() {
 	log = logging.NewLogger("VAULT_TRANSIT")
 }
 
-// MasterKey is a Vault Transit backend path used to encrypt and decrypt sops' data key.
-type MasterKey struct {
-	EncryptedKey string
-	KeyName      string
-	EnginePath   string
-	VaultAddress string
-	CreationDate time.Time
+var (
+	// log is the global logger for any Vault Transit MasterKey.
+	log *logrus.Logger
+	// vaultTTL is the duration after which a MasterKey requires rotation.
+	vaultTTL = time.Hour * 24 * 30 * 6
+	// defaultTokenFile is the name of the file in the user's home directory
+	// where a Vault token is expected to be stored.
+	defaultTokenFile = ".vault-token"
+)
+
+// Token used for authenticating towards a Vault server.
+type Token string
+
+// ApplyToMasterKey configures the token on the provided key.
+func (t Token) ApplyToMasterKey(key *MasterKey) {
+	key.token = string(t)
 }
 
-// NewMasterKeysFromURIs gets lots of keys from lots of URIs
+// MasterKey is a Vault Transit backend path used to Encrypt and Decrypt
+// SOPS' data key.
+type MasterKey struct {
+	// VaultAddress is the address of the Vault server.
+	VaultAddress string
+	// EnginePath is the path to the Vault Transit Secret engine relative
+	// to the VaultAddress.
+	EnginePath string
+	// KeyName is the name of the key in the Vault Transit engine.
+	KeyName string
+	// EncryptedKey contains the SOPS data key encrypted with the Vault Transit
+	// key.
+	EncryptedKey string
+	// CreationDate of the MasterKey, used to determine if the EncryptedKey
+	// needs rotation.
+	CreationDate time.Time
+
+	// token is the token used for authenticating against the VaultAddress
+	// server. It can be injected by a (local) keyservice.KeyServiceServer
+	// Token.ApplyToMasterKey. If empty, the default client configuration
+	// is used, before falling back to the token stored in defaultTokenFile.
+	token string
+}
+
+// NewMasterKeysFromURIs creates a list of MasterKeys from a list of Vault
+// URIs.
 func NewMasterKeysFromURIs(uris string) ([]*MasterKey, error) {
 	var keys []*MasterKey
 	if uris == "" {
 		return keys, nil
 	}
-	uriList := strings.Split(uris, ",")
-	for _, uri := range uriList {
+	for _, uri := range strings.Split(uris, ",") {
 		if uri == "" {
 			continue
 		}
@@ -54,9 +87,9 @@ func NewMasterKeysFromURIs(uris string) ([]*MasterKey, error) {
 	return keys, nil
 }
 
-// NewMasterKeyFromURI obtains the vaultAddress the transit backend path and the key name from the full URI of the key
+// NewMasterKeyFromURI obtains the Vault address, Transit backend path and the
+// key name from the full URI of the key.
 func NewMasterKeyFromURI(uri string) (*MasterKey, error) {
-	log.Debugln("Called NewMasterKeyFromURI with uri: ", uri)
 	var key *MasterKey
 	if uri == "" {
 		return key, nil
@@ -66,9 +99,10 @@ func NewMasterKeyFromURI(uri string) (*MasterKey, error) {
 		return nil, err
 	}
 	if u.Scheme == "" {
-		return nil, fmt.Errorf("missing scheme in vault URL (should be like this: https://vault.example.com:8200/v1/transit/keys/keyName), got: %v", uri)
+		return nil, fmt.Errorf("missing scheme in Vault URL (should be like this: +"+
+			"https://vault.example.com:8200/v1/transit/keys/keyName), got: %v", uri)
 	}
-	enginePath, keyName, err := getBackendAndKeyFromPath(u.RequestURI())
+	enginePath, keyName, err := engineAndKeyFromPath(u.RequestURI())
 	if err != nil {
 		return nil, err
 	}
@@ -77,110 +111,47 @@ func NewMasterKeyFromURI(uri string) (*MasterKey, error) {
 
 }
 
-func getBackendAndKeyFromPath(fullPath string) (enginePath, keyName string, err error) {
-	// Running vault behind a reverse proxy with longer urls seems not to be supported
-	// by the vault client api so we have a separate Error for that here.
-	if re := regexp.MustCompile(`/[^/]+/v[\d]+/[^/]+/[^/]+/[^/]+`); re.Match([]byte(fullPath)) {
-		return "", "", fmt.Errorf("running Vault with a prefixed url is not supported! (Format has to be like https://vault.example.com:8200/v1/transit/keys/keyName)")
-	} else if re := regexp.MustCompile(`/v[\d]+/[^/]+/[^/]+/[^/]+`); re.Match([]byte(fullPath)) == false {
-		return "", "", fmt.Errorf("vault path does not seem to be formatted correctly: (eg. https://vault.example.com:8200/v1/transit/keys/keyName)")
-	}
-	fullPath = strings.TrimPrefix(fullPath, "/")
-	fullPath = strings.TrimSuffix(fullPath, "/")
-
-	dirs := strings.Split(fullPath, "/")
-
-	keyName = dirs[len(dirs)-1]
-	enginePath = path.Join(dirs[1 : len(dirs)-2]...)
-	err = nil
-	return
-}
-
-// NewMasterKey creates a new MasterKey from a vault address, transit backend path and a key name and setting the creation date to the current date
-func NewMasterKey(addess, enginePath, keyName string) *MasterKey {
-	mk := &MasterKey{
-		VaultAddress: addess,
+// NewMasterKey creates a new MasterKey from a Vault address, Transit backend
+// path and a key name.
+func NewMasterKey(address, enginePath, keyName string) *MasterKey {
+	key := &MasterKey{
+		VaultAddress: address,
 		EnginePath:   enginePath,
 		KeyName:      keyName,
 		CreationDate: time.Now().UTC(),
 	}
-	log.Debugln("Created Vault Master Key: ", mk)
-	return mk
+	return key
 }
 
-// EncryptedDataKey returns the encrypted data key this master key holds
-func (key *MasterKey) EncryptedDataKey() []byte {
-	return []byte(key.EncryptedKey)
-}
-
-// SetEncryptedDataKey sets the encrypted data key for this master key
-func (key *MasterKey) SetEncryptedDataKey(enc []byte) {
-	key.EncryptedKey = string(enc)
-}
-
-func vaultClient(address string) (*api.Client, error) {
-	cfg := api.DefaultConfig()
-	cfg.Address = address
-	cli, err := api.NewClient(cfg)
-	if err != nil {
-		return nil, fmt.Errorf("Cannot create Vault Client: %w", err)
-	}
-	if cli.Token() != "" {
-		return cli, nil
-	}
-	homePath, err := homedir.Dir()
-	if err != nil {
-		panic(fmt.Sprintf("error getting user's home directory: %v", err))
-	}
-	tokenPath := filepath.Join(homePath, ".vault-token")
-	f, err := os.Open(tokenPath)
-	if os.IsNotExist(err) {
-		return cli, nil
-	}
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-
-	buf := bytes.NewBuffer(nil)
-	if _, err := io.Copy(buf, f); err != nil {
-		return nil, err
-	}
-	cli.SetToken(strings.TrimSpace(buf.String()))
-	return cli, nil
-}
-
-// Encrypt takes a sops data key, encrypts it with Vault Transit and stores the result in the EncryptedKey field
+// Encrypt takes a SOPS data key, encrypts it with Vault Transit, and stores
+// the result in the EncryptedKey field.
 func (key *MasterKey) Encrypt(dataKey []byte) error {
-	fullPath := path.Join(key.EnginePath, "encrypt", key.KeyName)
-	cli, err := vaultClient(key.VaultAddress)
+	fullPath := key.encryptPath()
+
+	client, err := vaultClient(key.VaultAddress, key.token)
 	if err != nil {
+		log.WithError(err).WithField("Path", fullPath).Error("Encryption failed")
 		return err
 	}
-	encoded := base64.StdEncoding.EncodeToString(dataKey)
-	payload := make(map[string]interface{})
-	payload["plaintext"] = encoded
-	raw, err := cli.Logical().Write(fullPath, payload)
+
+	secret, err := client.Logical().Write(fullPath, encryptPayload(dataKey))
 	if err != nil {
-		log.WithField("Path", fullPath).Info("Encryption failed")
-		return err
+		log.WithError(err).WithField("Path", fullPath).Error("Encryption failed")
+		return fmt.Errorf("failed to encrypt sops data key to Vault transit backend '%s': %w", fullPath, err)
 	}
-	if raw == nil || raw.Data == nil {
-		return fmt.Errorf("The transit backend %s is empty", fullPath)
+	encryptedKey, err := encryptedKeyFromSecret(secret)
+	if err != nil {
+		log.WithError(err).WithField("Path", fullPath).Error("Encryption failed")
+		return fmt.Errorf("failed to encrypt sops data key to Vault transit backend '%s': %w", fullPath, err)
 	}
-	encrypted, ok := raw.Data["ciphertext"]
-	if !ok {
-		return fmt.Errorf("there's not encrypted data")
-	}
-	encryptedKey, ok := encrypted.(string)
-	if !ok {
-		return fmt.Errorf("the ciphertext cannot be casted to string")
-	}
+
 	key.EncryptedKey = encryptedKey
+	log.WithField("Path", fullPath).Info("Encryption successful")
 	return nil
 }
 
-// EncryptIfNeeded encrypts the provided sops' data key and encrypts it if it hasn't been encrypted yet
+// EncryptIfNeeded encrypts the provided SOPS data key, if it has not been
+// encrypted yet.
 func (key *MasterKey) EncryptIfNeeded(dataKey []byte) error {
 	if key.EncryptedKey == "" {
 		return key.Encrypt(dataKey)
@@ -188,81 +159,53 @@ func (key *MasterKey) EncryptIfNeeded(dataKey []byte) error {
 	return nil
 }
 
+// EncryptedDataKey returns the encrypted data key this master key holds.
+func (key *MasterKey) EncryptedDataKey() []byte {
+	return []byte(key.EncryptedKey)
+}
+
+// SetEncryptedDataKey sets the encrypted data key for this master key.
+func (key *MasterKey) SetEncryptedDataKey(enc []byte) {
+	key.EncryptedKey = string(enc)
+}
+
 // Decrypt decrypts the EncryptedKey field with Vault Transit and returns the result.
 func (key *MasterKey) Decrypt() ([]byte, error) {
-	fullPath := path.Join(key.EnginePath, "decrypt", key.KeyName)
-	cli, err := vaultClient(key.VaultAddress)
+	fullPath := key.decryptPath()
+
+	client, err := vaultClient(key.VaultAddress, key.token)
 	if err != nil {
+		log.WithError(err).WithField("Path", fullPath).Error("Decryption failed")
 		return nil, err
 	}
-	payload := make(map[string]interface{})
-	payload["ciphertext"] = key.EncryptedKey
-	raw, err := cli.Logical().Write(fullPath, payload)
+
+	secret, err := client.Logical().Write(fullPath, decryptPayload(key.EncryptedKey))
 	if err != nil {
-		log.WithField("Path", fullPath).Info("Encryption failed")
-		return nil, err
+		log.WithError(err).WithField("Path", fullPath).Error("Decryption failed")
+		return nil, fmt.Errorf("failed to decrypt sops data key from Vault transit backend '%s': %w", fullPath, err)
 	}
-	if raw == nil || raw.Data == nil {
-		return nil, fmt.Errorf("The transit backend %s is empty", fullPath)
-	}
-	decrypted, ok := raw.Data["plaintext"]
-	if ok != true {
-		return nil, fmt.Errorf("there's no decrypted data")
-	}
-	dataKey, ok := decrypted.(string)
-	if ok != true {
-		return nil, fmt.Errorf("the plaintest cannot be casted to string")
-	}
-	result, err := base64.StdEncoding.DecodeString(dataKey)
+	dataKey, err := dataKeyFromSecret(secret)
 	if err != nil {
-		return nil, fmt.Errorf("Couldn't decode base64 plaintext")
+		log.WithError(err).WithField("Path", fullPath).Error("Decryption failed")
+		return nil, fmt.Errorf("failed to decrypt sops data key from Vault transit backend '%s': %w", fullPath, err)
 	}
-	return result, nil
+
+	log.WithField("Path", fullPath).Info("Decryption successful")
+	return dataKey, nil
 }
 
 // NeedsRotation returns whether the data key needs to be rotated or not.
-// This is simply copied from GCPKMS
-// TODO: handle key rotation on vault side
 func (key *MasterKey) NeedsRotation() bool {
-	//TODO: manage rewrapping https://www.vaultproject.io/api/secret/transit/index.html#rewrap-data
-	return time.Since(key.CreationDate) > (time.Hour * 24 * 30 * 6)
+	// TODO: manage rewrapping https://www.vaultproject.io/api/secret/transit/index.html#rewrap-data
+	return time.Since(key.CreationDate) > (vaultTTL)
 }
 
-// ToString converts the key to a string representation
+// ToString converts the key to a string representation.
 func (key *MasterKey) ToString() string {
 	return fmt.Sprintf("%s/v1/%s/keys/%s", key.VaultAddress, key.EnginePath, key.KeyName)
 }
 
-func (key *MasterKey) createVaultTransitAndKey() error {
-	cli, err := vaultClient(key.VaultAddress)
-	if err != nil {
-		return err
-	}
-	if err != nil {
-		return fmt.Errorf("Cannot create Vault Client: %w", err)
-	}
-	err = cli.Sys().Mount(key.EnginePath, &api.MountInput{
-		Type:        "transit",
-		Description: "backend transit used by SOPS",
-	})
-	if err != nil {
-		return err
-	}
-	path := path.Join(key.EnginePath, "keys", key.KeyName)
-	payload := make(map[string]interface{})
-	payload["type"] = "rsa-4096"
-	_, err = cli.Logical().Write(path, payload)
-	if err != nil {
-		return err
-	}
-	_, err = cli.Logical().Read(path)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-// ToMap converts the MasterKey to a map for serialization purposes
+// ToMap converts the MasterKey to a map for serialization purposes.
 func (key MasterKey) ToMap() map[string]interface{} {
 	out := make(map[string]interface{})
 	out["vault_address"] = key.VaultAddress
@@ -271,4 +214,146 @@ func (key MasterKey) ToMap() map[string]interface{} {
 	out["enc"] = key.EncryptedKey
 	out["created_at"] = key.CreationDate.UTC().Format(time.RFC3339)
 	return out
+}
+
+// encryptPath returns the path for Encrypt requests.
+func (key *MasterKey) encryptPath() string {
+	return path.Join(key.EnginePath, "encrypt", key.KeyName)
+}
+
+// decryptPath returns the path for Decrypt requests.
+func (key *MasterKey) decryptPath() string {
+	return path.Join(key.EnginePath, "decrypt", key.KeyName)
+}
+
+// encryptPayload returns the payload for an encrypt request of the dataKey.
+func encryptPayload(dataKey []byte) map[string]interface{} {
+	encoded := base64.StdEncoding.EncodeToString(dataKey)
+	return map[string]interface{}{
+		"plaintext": encoded,
+	}
+}
+
+// encryptedKeyFromSecret attempts to extract the encrypted key from the data
+// of the provided secret.
+func encryptedKeyFromSecret(secret *api.Secret) (string, error) {
+	if secret == nil || secret.Data == nil {
+		return "", fmt.Errorf("transit backend is empty")
+	}
+	encrypted, ok := secret.Data["ciphertext"]
+	if !ok {
+		return "", fmt.Errorf("no encrypted data")
+	}
+	encryptedKey, ok := encrypted.(string)
+	if !ok {
+		return "", fmt.Errorf("encrypted ciphertext cannot be cast to string")
+	}
+	return encryptedKey, nil
+}
+
+// decryptPayload returns the payload for a decrypt request of the
+// encryptedKey.
+func decryptPayload(encryptedKey string) map[string]interface{} {
+	return map[string]interface{}{
+		"ciphertext": encryptedKey,
+	}
+}
+
+// dataKeyFromSecret attempts to extract the data key from the data of the
+// provided secret.
+func dataKeyFromSecret(secret *api.Secret) ([]byte, error) {
+	if secret == nil || secret.Data == nil {
+		return nil, fmt.Errorf("transit backend is empty")
+	}
+	decrypted, ok := secret.Data["plaintext"]
+	if !ok {
+		return nil, fmt.Errorf("no decrypted data")
+	}
+	plaintext, ok := decrypted.(string)
+	if !ok {
+		return nil, fmt.Errorf("decrypted plaintext data cannot be cast to string")
+	}
+	dataKey, err := base64.StdEncoding.DecodeString(plaintext)
+	if err != nil {
+		return nil, fmt.Errorf("cannot decode base64 plaintext into data key bytes")
+	}
+	return dataKey, nil
+}
+
+// vaultClient returns a new Vault client, configured with the given address
+// and token.
+func vaultClient(address, token string) (*api.Client, error) {
+	cfg := api.DefaultConfig()
+	cfg.Address = address
+
+	client, err := api.NewClient(cfg)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create Vault client: %w", err)
+	}
+
+	if token != "" {
+		client.SetToken(token)
+	}
+	// Provided token takes precedence over the user's token.
+	if client.Token() == "" {
+		if token, err = userVaultToken(); err != nil {
+			return nil, fmt.Errorf("cannot get Vault token: %w", err)
+		}
+		if token != "" {
+			client.SetToken(token)
+		}
+	}
+
+	return client, nil
+}
+
+// userVaultsToken returns the token from `$HOME/.vault-token` if the file
+// exists. It returns an error if the file exists but cannot be read from.
+// If the file does not exist, it returns an empty string.
+func userVaultToken() (string, error) {
+	homePath, err := homedir.Dir()
+	if err != nil {
+		return "", fmt.Errorf("error getting user's home directory: %w", err)
+	}
+	tokenPath := filepath.Join(homePath, defaultTokenFile)
+
+	f, err := os.Open(tokenPath)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return "", nil
+		}
+		return "", err
+	}
+	defer f.Close()
+
+	buf := bytes.NewBuffer(nil)
+	if _, err := io.Copy(buf, f); err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(buf.String()), nil
+}
+
+// engineAndKeyFromPath returns the engine path and key name from the full
+// path, or an error.
+func engineAndKeyFromPath(fullPath string) (enginePath, keyName string, err error) {
+	// Running vault behind a reverse proxy with longer URLs seems not to be
+	// supported by the Vault client API. Check for this here.
+	// TODO(hidde): this may no longer be necessary with newer Vault versions,
+	//  but needs to be confirmed.
+	if re := regexp.MustCompile(`/[^/]+/v[\d]+/[^/]+/[^/]+/[^/]+`); re.Match([]byte(fullPath)) {
+		err = fmt.Errorf("running Vault with a prefixed URL is not supported! (Format has to be like " +
+			"https://vault.example.com:8200/v1/transit/keys/keyName)")
+		return
+	} else if re := regexp.MustCompile(`/v[\d]+/[^/]+/[^/]+/[^/]+`); !re.Match([]byte(fullPath)) {
+		err = fmt.Errorf("vault path does not seem to be formatted correctly: (eg. " +
+			"https://vault.example.com:8200/v1/transit/keys/keyName)")
+		return
+	}
+
+	fullPath = strings.Trim(fullPath, "/")
+	dirs := strings.Split(fullPath, "/")
+
+	keyName = dirs[len(dirs)-1]
+	enginePath = path.Join(dirs[1 : len(dirs)-2]...)
+	return
 }
