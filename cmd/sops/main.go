72727aa17e1764a9bf2c42a761b9768cd70290bb
sledzikowy <48012758+sledzikowy@users.noreply.github.com>
2024-12-13 13:06:30 +0100

    Add abbility to set sops config location via envvar
    Signed-off-by: sledzikowy <48012758+sledzikowy@users.noreply.github.com>
    Signed-off-by: Arun Bhagyanath <a.bhagyanath@eclecticiq.com>

--- cmd/sops/main.go
+++ cmd/sops/main.go
@@ -1682,6 +1682,7 @@ func main() {
 		cli.StringFlag{
 			Name:  "config",
 			Usage: "path to sops' config file. If set, sops will not search for the config file recursively.",
+			EnvVar: "SOPS_CONFIG",
 		},
 		cli.StringFlag{
 			Name:  "encryption-context",
