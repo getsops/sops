c288864a246b3007f58c391e1f81b6266a84d079
Maren Sofie Ringsby <marensofieringsby@gmail.com>
2025-03-05 08:36:23 +0100

    remove unnecessary files and change go.mod
    Signed-off-by: Maren Sofie Ringsby <marensofieringsby@gmail.com>

--- functional-tests/target/debug/build/serde-e4a60933da51b34b/build_script_build-e4a60933da51b34b.d
+++
@@ -1,5 +0,0 @@
-/Users/marensofieringsby/Documents/code/bekk/sops/functional-tests/target/debug/build/serde-e4a60933da51b34b/build_script_build-e4a60933da51b34b: /Users/marensofieringsby/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.218/build.rs
-
-/Users/marensofieringsby/Documents/code/bekk/sops/functional-tests/target/debug/build/serde-e4a60933da51b34b/build_script_build-e4a60933da51b34b.d: /Users/marensofieringsby/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.218/build.rs
-
-/Users/marensofieringsby/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.218/build.rs:
