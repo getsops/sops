ea6d7ed8bea5a98a9caba8d1c249ea58f5db4308
Arun Bhagyanath <arunbhagyanath@gmail.com>
2025-08-04 19:10:51 +0200

    Add support for OVH KMS in SOPS
    Signed-off-by: Arun Bhagyanath <arunbhagyanath@gmail.com>

--- stores/flatten.go
+++ stores/flatten.go
@@ -248,7 +248,14 @@ func DecodeNonStrings(m map[string]interface{}) error {
 		case string:
 			vInt, err := strconv.Atoi(val)
 			if err != nil {
-				return fmt.Errorf("shamir_threshold is not an integer: %s", err.Error())
+				// Older versions of SOPS stored shamir_threshold as a floating point representation
+				// of the actual integer. Try to parse a floating point number and see whether it
+				// can be converted without loss to an integer.
+				vFloat, floatErr := strconv.ParseFloat(val, 64)
+				vInt = int(vFloat)
+				if floatErr != nil || float64(vInt) != vFloat {
+					return fmt.Errorf("shamir_threshold is not an integer: %s", err.Error())
+				}
 			}
 			m["shamir_threshold"] = vInt
 		case int:
@@ -273,6 +280,12 @@ func EncodeNonStrings(m map[string]interface{}) {
 	if v, found := m["shamir_threshold"]; found {
 		if vInt, ok := v.(int); ok {
 			m["shamir_threshold"] = fmt.Sprintf("%d", vInt)
+		}
+		// FlattenMetadata serializes the input as JSON and then deserializes it.
+		// The JSON unserializer treats every number as a float, so the above 'if'
+		// never applies in that situation.
+		if vFloat, ok := v.(float64); ok {
+			m["shamir_threshold"] = fmt.Sprintf("%.0f", vFloat)
 		}
 	}
 }
